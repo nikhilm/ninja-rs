85dd9bc044659824316c3f83ffba1992024a8e32
Nikhil Marathe <nsm.nikhil@gmail.com>
2020-08-04 18:59:12 -0700

    add a license notice to all rust files

--- parse/tests/parse_test.rs
+++ parse/tests/parse_test.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Nikhil Marathe <nsm.nikhil@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 use insta::{assert_debug_snapshot, assert_display_snapshot};
 use ninja_parse::{build_representation, Loader};
 use std::{ffi::OsStr, fs, os::unix::ffi::OsStrExt, path::Path};
